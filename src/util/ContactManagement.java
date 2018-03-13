3b1fabdd76f9ca244ae066a14e08cd156d0fe4bd
TMcCool <tjmccool1@icloud.com>
2018-03-13 10:55:50 -0500

    modified:   contacts.txt cannot have a line without # symbol  modified:   src/util/ContactManagement.java updated some functionalality still still no delete print does not update after running add.????????

--- src/util/ContactManagement.java
+++ src/util/ContactManagement.java
@@ -1,45 +1,54 @@
 package util;
 
 
-import util.Input;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class ContactManagement {
 
     public static Input ask = new Input();
-    public static String path = "src/contacts.txt";
+    public static Path path = Paths.get("contacts.txt");
 
-    public static void mainMenu(){
+    public static void mainMenu() {
+//        do {
         System.out.println("1. View Contacts");
         System.out.println("2. Add a new Contact");
         System.out.println("3. Search a contact by name");
         System.out.println("4. Delete an existing contact");
         System.out.println("5. Exit");
         System.out.println("Enter an option (1,2,3,4 or 5)");
+//        }
     }
+
+
 
     public static List<String> slurp(){
         try {
-            return Files.readAllLines(Paths.get(path));
+
+            System.out.println("slurp good to go");
+            return Files.readAllLines(Paths.get(String.valueOf(path)));
+
         }catch (IOException e){
             System.out.println(e);
             System.exit(1);
             return null;
         }
     }
-    public static void viewContacts(){
-        List<String> contacts = slurp();
-        printContacts(contacts);
+    public static List<String> contacts;
+    public static List<String> viewContacts(){
+        contacts = slurp();
+//        System.out.println(contacts);
+        return contacts;
     }
 
     public static void printContacts(List<String> contacts){
+//        viewContacts();
         System.out.printf("%-15s | %s\n", "First Last", "PhoneNum");
         System.out.println("-----------------------------");
         for(String contact: contacts){
@@ -50,14 +59,14 @@ public class ContactManagement {
         }
     }
 
-
     public static void addContact(){
         List<String> newEntry = new ArrayList<>();
         String entry = ask.getString("Enter your name: (First Last)") +" #"+ ask.getInt("Enter your phone number: ");
         newEntry.add(entry);
 
         try {
-            Files.write(Paths.get(path), newEntry, StandardOpenOption.APPEND);
+            //            modified from path variable
+            Files.write(Paths.get(String.valueOf(path)), newEntry, StandardOpenOption.APPEND);
         }catch (IOException e){
             System.out.println("Who Care!");
         }
@@ -71,7 +80,16 @@ public class ContactManagement {
                 results.add(contact);
             }
         }
-        printContacts(results);
+//        printContacts(results);
+    }
+
+    public static void main(String[] args) {
+        slurp();
+        viewContacts();
+        printContacts(contacts);
+        addContact();
+        System.out.println("--reprint of contacts--------");
+        printContacts(contacts);
     }
 
 }
